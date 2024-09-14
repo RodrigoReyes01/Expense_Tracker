88ae1ff59a318f97dc9b518662d4d8c19e696b54
Rodrigo Reyes <71049819+RodrigoReyes01@users.noreply.github.com>
2024-09-14 10:53:37 -0600

    Final Template Update
    Added Chart and legends etc

--- Expense Tracker/Expense Tracker/ContentView.swift
+++ Expense Tracker/Expense Tracker/ContentView.swift
@@ -6,16 +6,39 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct ContentView: View {
+    @EnvironmentObject var transactionListVM: TransactionListViewModel
+//    var demoData: [Double] = [8, 2, 4, 6, 12, 9, 2]
+    
     var body: some View {
         NavigationView {
-            ScrollView{
+             ScrollView{
                 VStack(alignment: .leading, spacing: 24) {
                     //MARK: Title
                     Text("Overview")
                         .font(.title2)
                         .bold()
+                    
+                    //MARK: Chart
+                    let data = transactionListVM.accumulateTransactions()
+                    
+                    if !data.isEmpty{
+                        let totalExpenses = data.last?.1 ?? 0
+                        
+                        CardView {
+                            VStack(alignment: .leading) {
+                                ChartLabel(totalExpenses.formatted(.currency(code: "USD")), type: .title, format: "$%.02f")
+                                
+                                LineChart()
+                            }
+                            .background(Color.systemBackground)
+                        }
+                        .data(data)
+                        .chartStyle(ChartStyle(backgroundColor: Color.systemBackground, foregroundColor: ColorGradient(Color.Icon.opacity(0.4), Color.Icon)))
+                        .frame(height: 300)
+                    }
                     
                         //MARK: Transaction List
                     RecentTransactionList()
